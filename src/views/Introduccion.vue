2c5fd33e640ddeef0886ce3e696be79cfb175987
Luis Perez <ljpm9401@gmail.com>
2024-11-22 18:57:37 -0500

    Avances

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -10,7 +10,7 @@
     
     p(data-aos="fade-down" data-aos-duration="900") Un circuito eléctrico es una interconexión de elementos que permite el flujo de corriente para realizar diversas funciones en sistemas eléctricos y electrónicos. Estos componentes, como resistencias, condensadores, inductores y semiconductores, se organizan en configuraciones específicas, generando trayectorias cerradas donde la energía puede circular y transformarse según sea necesario.
 
-    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mt-4(data-aos="flip" data-aos-duration="900")
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mt-4(data-aos="flip-up" data-aos-duration="900")
       .bloque-texto-g__img(
         :style="{'background-image': `url(${require('@/assets/curso/intro1.png')})`}"
       )
@@ -18,6 +18,15 @@
         figure.mb-2.d-block
           img(src="@/assets/curso/intro2.svg" style="max-width:106px;")
         p.mb-0 Existen diferentes tipos de circuitos, clasificados en función de la corriente que emplean, ya sea directa o alterna; y de la disposición de sus elementos, que puede ser en serie, paralelo o mixta. Cada configuración impacta en la forma en que se distribuyen el voltaje y la corriente, adaptando el circuito a necesidades específicas en aplicaciones domésticas e industriales.
+    
+    .col-xl-10.m-auto.mt-5
+      .p-4.p-lg-3(style="background-color:#DDE9F7;" data-aos="fade-up" data-aos-duration="750")
+        .row
+          .col-md-auto.d-none.d-md-block(style="place-self:center;")
+            figure
+              img(src="@/assets/curso/intro3.svg")
+          .col-md(style="place-self:center;")
+            p.mb-0 La estructura y los componentes de un circuito determinan su comportamiento y aplicabilidad en diversas áreas de la tecnología. Los elementos pasivos y activos, como diodos y transistores, permiten controlar, amplificar y transformar la energía, haciendo de los circuitos eléctricos una base fundamental para el funcionamiento de sistemas electrónicos modernos.
 
     
 </template>
